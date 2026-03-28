2d898eb1c3ed7f6af3e454538ef8d2ddbbf6355e
tir <Jamal@company>
2026-03-28 13:18:20 +0300

    создал новый проект

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿using CACTOC;
+
+var david = new User("davidsuper225","david.com","kniga228");
+var danil = new User();
---
+++ user.cs
@@ -0,0 +1,22 @@
+namespace CACTOC;
+
+class User
+{
+    public string UserName{get; set;} = string.Empty;
+    public string Password{get; set;} = string.Empty;
+    public string Email {get;set;} = string.Empty;
+
+
+    public User(string name, string email, string password)
+    {
+        UserName = name;
+        Password = password;
+        Email = email;
+        System.Console.WriteLine($"{UserName} ");
+
+    }
+
+
+    public User() :
+        this("данил колбасенко", "danilkolbasenko", "kolbasenco"){}
+ }
